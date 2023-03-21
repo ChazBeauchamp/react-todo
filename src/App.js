edcaab6696a0f45e7bb20ebd75bf77574c975dfa
ChazBeauchamp <chazbeauchamp1@gmail.com>
2023-03-21 16:32:24 -0400

    began creating unordered list of to do items

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React from "react";
+
+const todoList = [
+  {
+    id: 1,
+    title: "Read Road to React",
+  },
+  {
+    id: 2,
+    title: "Complete Coding Assignment",
+  },
+  {
+    id: 3,
+    title: "Submit Pull Request Link",
+  },
+  {
+    id: 4,
+    title: "Celebrate",
+  },
+];
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todoList.map(function (item) {
+          return (
+            <li key={item.id}>
+              <span>{item.title}</span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
